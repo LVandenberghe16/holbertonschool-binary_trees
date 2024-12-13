68ac46457d576f015c89b418a6fdebecd6711a52
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-12-13 11:45:39 +0100

    oui

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,31 +1,59 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: The height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ *
+ * Return: The size of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
  *
- * Description: A binary tree is perfect if all internal nodes
- * have two children and all leaf nodes are at the same level.
- * If the tree is NULL, it is not considered perfect.
+ * Description: A perfect binary tree is a tree where all the internal nodes
+ * have two children and all leaves are at the same level.
  *
- * Return: 1 if the tree is perfect, otherwise 0.
+ * Return: 1 if the tree is perfect, 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t size, i, j, sum = 0, pow;
+	size_t height, size, expected_size;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
+	height = binary_tree_height(tree);
 	size = binary_tree_size(tree);
 
-	for (i = 0; i <= binary_tree_height(tree); i++)
-	{
-		for (j = 0, pow = 1; j < i; j++)
-			pow *= 2;
-		sum += pow;
-	}
-	if (sum == size)
-		return (1);
-	return (0);
+	/* Expected size of a perfect binary tree: 2^(height) - 1 */
+	expected_size = (1 << height) - 1;
+
+	return (size == expected_size);
 }
